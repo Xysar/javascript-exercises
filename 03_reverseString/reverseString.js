bbcea1469c0fd864ad9e4962fe88f81afec327aa
Cesar Camacho <cesarcamacho760@gmail.com>
2022-06-22 13:49:52 -0700

    Complete reverseString and removefromArray

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+let res = "";
+let len = str.length;
+for(let i =0; i< len ; i++){
+    let character = str.charAt(0);
+    res = character + res; 
+    str = str.substring(1);
+}
+return res;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array , ...args) {
+args.forEach(element => {
+    array = array.filter(function(ele){
+        return ele !== element;
+    })
+});
+       
+    return array;
 };
 
 // Do not edit below this line
