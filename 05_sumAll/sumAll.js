da03251cd6661c2df2e5fb87ee1e307c6d03aefe
Cesar Camacho <cesarcamacho760@gmail.com>
2022-06-23 10:55:40 -0700

    Complete sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function (first, second) {
+  if (typeof(first) != "number" || typeof(second) !="number") {
+    return "ERROR";
+  }
+  if (first < 0 || second < 0) {
+    return "ERROR";
+  }
+  if (first > second) {
+    let temp = second;
+    second = first;
+    first = temp;
+  }
 
+  let sum = 0;
+  for(;first<=second; first++){
+    sum+=first;
+  }
+  return sum;
 };
 
 // Do not edit below this line
