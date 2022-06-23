0dd2d7f8bf17565021edea412f9331b0fa87948f
Cesar Camacho <cesarcamacho760@gmail.com>
2022-06-23 12:41:47 -0700

    Complete leapYear and tempConversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function(degrees) {
+degrees -= 32;
+degrees *= (5/9);
+degrees = Math.round(degrees *10) / 10;
+return degrees;
 };
 
-const ctof = function() {
-
+const ctof = function(degrees) {
+degrees *= (9/5);
+degrees += 32;
+degrees = Math.round(degrees *10) / 10;
+return degrees;
 };
 
 // Do not edit below this line
